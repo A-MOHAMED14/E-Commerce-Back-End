3bd9b80e4d9c66078a9a5a97ca4b4a8e5a2cc55b
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-16 19:23:36 +0100

    Add route to delete a single tag with it's id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Tag, Product, ProductTag } = require("../../models");
+const { destroy } = require("../../models/Category");
 
 // The `/api/tags` endpoint
 
@@ -55,8 +56,21 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
