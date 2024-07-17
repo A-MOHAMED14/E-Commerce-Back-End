e0be5ef8787a1bbd612f426455f1e3281bfd55af
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-17 12:11:13 +0100

    Refactor: attach Category model to findByPk and use res.status for status code 200

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const categoryData = await findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
@@ -25,7 +25,7 @@ router.get("/:id", async (req, res) => {
       res.status(404).json({ message: "No category found with this id!" });
       return;
     }
-    res.json(200).json(categoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
