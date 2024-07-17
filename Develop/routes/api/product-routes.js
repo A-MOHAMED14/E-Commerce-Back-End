bc4e5a25cc9aa909ec1a6cc89543adda5ce27153
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-17 13:56:24 +0100

    Send status code and product data on successful product deletion by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -116,6 +116,7 @@ router.delete("/:id", async (req, res) => {
       res.status(400).json({ message: "No product found with this id!" });
       return;
     }
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
